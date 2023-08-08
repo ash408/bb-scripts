f30e914787c14043cdee0c294e9f93de2917dbb9
Jay Doe <124532093+ash408@users.noreply.github.com>
2023-08-08 10:38:23 +0000

    Completed automatic connection system

--- break.js
+++ break.js
@@ -1,13 +1,13 @@
 /** @param {NS} ns */
 export async function main(ns) {
   const server = ns.args[0];
-  const currentHost = ns.args[1];
 
   if (server && ns.serverExists(server)) {
     ns.nuke(server);
+    ns.tprint(`Breaking ${server}`);
 
-    await ns.scp("scripts/intruder.js", server);
-    ns.exec("scripts/intruder.js", server, 1, server);
+    ns.tprint(`Spawning new scanner on ${server}`)
+    ns.spawn("scripts/scan.js", 1, server);
   }
 
   else {
--- scan.js
+++ scan.js
@@ -2,10 +2,14 @@
 export async function main(ns) {
   const server = ns.args[0];
 
+  ns.tprint(`Running scan from ${server}`)
+
    if (server){
     const connections = ns.scan(server);
 
     for (let connection of connections){
+      ns.tprint(`Discovered ${connection} from ${server}`)
+
       const hackLevel = ns.getHackingLevel();
       const serverLevel = ns.getServerRequiredHackingLevel(connection);
 
@@ -13,8 +17,9 @@ export async function main(ns) {
         const requiredPorts = ns.getServerNumPortsRequired(connection);
 
         if (requiredPorts == 0){
-          await ns.scp("scripts/break.js", connection);
-          ns.exec("scripts/break.js", connection);
+          ns.tprint(`Connecting to ${connection}`);
+          await ns.scp(["scripts/break.js", "scripts/scan.js", "scripts/thief.js"], connection);
+          ns.exec("scripts/break.js", connection, 1, connection);
         }
       }
     }
